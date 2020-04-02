e61d63c2f6289b57cd5f082e2a15a8996410df8e
Nikolai Trusov <trusovn@yahoo.com>
2020-04-02 16:52:37 +0300

    Initical setup cont. 2. wip.

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] [Scene] string startScene = default;
+    [SerializeField] [Scene] string gameOverScene = default;
+    [SerializeField] [Scene] string firstGameScene = default;
+
+    public void LoadGameOver()
+    {
+        SceneManager.LoadScene(gameOverScene);
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(firstGameScene);
+        //var gameController = FindObjectOfType<GameController>();
+        //if (gameController)
+        //{
+        //    gameController.ResetGame();
+        //}
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(startScene);
+    }
+
+    public void QuitApp()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
