b094f304c1a67782104a3c604fc9e131fe905073
Nikolai Trusov <trusovn@yahoo.com>
2020-04-08 19:40:29 +0300

    Adding potato projectile

---
+++ Assets/Scripts/Defender.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Defender : MonoBehaviour
+{
+    [SerializeField] GameObject attackProjectile;
+    [SerializeField] Vector2 projectileSpawnOffset;
+
+    public void Attack()
+    {
+        if (attackProjectile)
+        {
+            Instantiate(attackProjectile, transform.position + new Vector3(projectileSpawnOffset.x, projectileSpawnOffset.y, 0), Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] float flySpeed = 1;
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * flySpeed * Time.deltaTime, Space.World);    
+    }
+}
