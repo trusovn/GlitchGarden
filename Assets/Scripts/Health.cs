696b641bbe2792f71cc30a620eff5504f7ad64fd
Nikolai Trusov <trusovn@yahoo.com>
2020-04-08 20:05:52 +0300

    Projectile deals damage and kills attackers

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] int healthPoints = 10;
+
+    public void GotDamage(int points)
+    {
+        healthPoints -= points;
+        if (healthPoints <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -3,9 +3,24 @@
 public class Projectile : MonoBehaviour
 {
     [SerializeField] float flySpeed = 1;
+    [SerializeField] int damagePoints = 25;
 
     void Update()
     {
         transform.Translate(Vector2.right * flySpeed * Time.deltaTime, Space.World);    
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Attacker>())
+        {
+            collision.GetComponent<Health>().GotDamage(damagePoints);
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
     }
 }
