0dad840e629c7c55213d4d07307f3bd3720552fd
Nikolai Trusov <trusovn@yahoo.com>
2020-04-09 00:30:42 +0300

    Spawn defenders on mouse click

---
+++ Assets/Game.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField] Defender defender = default;
+    
+    private void OnMouseDown()
+    {
+        var position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Instantiate(defender, new Vector2(position.x, position.y), Quaternion.identity);
+    }
+}
