32869cf58307210b199b64bdb29da17c959d7361
Nikolai Trusov <trusovn@yahoo.com>
2020-04-10 16:05:45 +0300

    Sorting script with custom editor for sorting layer child sprites

---
+++ Assets/Scripts/Editor/SortingEditor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Linq;
+
+[CustomEditor(typeof(Sorting))]
+public class SortingEditor : Editor
+{
+    Sorting sorting;
+
+    private void OnEnable()
+    {
+        sorting = (Sorting)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        GUI.changed = false;
+        var layers = SortingLayer.layers.Select(x => x.name).ToArray();
+        var selected = EditorGUILayout.Popup("Sorting Layer", sorting.SortingValue, layers);
+        if (GUI.changed)
+        {
+            sorting.UpdateChildrenSortingLayer(layers[selected]);
+        }
+    }
+}
---
+++ Assets/Scripts/Sorting.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Sorting : MonoBehaviour
+{
+    int sortingValue;
+
+    public int SortingValue { get => sortingValue; }
+
+    public void UpdateChildrenSortingLayer(string layerName)
+    {
+        sortingValue = SortingLayer.GetLayerValueFromName(layerName);
+        var sprites = GetComponentsInChildren<SpriteRenderer>();
+        Debug.Log(layerName);
+        foreach (var sr in sprites)
+        {
+            sr.sortingLayerName = layerName;
+            Debug.Log(sr.gameObject.name);
+        }
+    }
+}
