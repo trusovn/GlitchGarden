c0dc46020e1e7b12a0e979130bcebd18fcf12485
Nikolai Trusov <trusovn@yahoo.com>
2020-04-11 01:08:25 +0300

    Adding star spawn. WIP.
    Star prefab.
    Star spawn animation.

---
+++ Assets/Scripts/StarSpawner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class StarSpawner : MonoBehaviour
+{
+    [SerializeField] int starPoints = default;
+    [SerializeField] float spawnEveryNSeconds = default;
+    [SerializeField] string animationStarSpawnTrigger = "NewStar";
+    [SerializeField] Transform starSpawnPosition = default;
+    //public void AddStarsToDisplay()
+    //{
+    //    addStarsMethod.Invoke(starCosts);
+    //}
+
+    public void SpawnStar()
+    {
+        
+    }
+}
