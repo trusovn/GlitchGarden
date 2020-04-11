7899c79118a9d43f88013d54dd85736e9eb0430a
Nikolai Trusov <trusovn@yahoo.com>
2020-04-11 19:22:04 +0300

    Place defender only in empty cell

--- Assets/Scripts/DefenderSpawner.cs
+++ Assets/Scripts/DefenderSpawner.cs
@@ -3,7 +3,15 @@
 public class DefenderSpawner : MonoBehaviour
 {
     Defender defender;
-    
+    bool[] cells;
+    Vector2 fieldSize;
+
+    private void Start()
+    {
+        fieldSize = GetComponent<BoxCollider2D>().size;
+        cells = new bool[(int)(fieldSize.x * fieldSize.y)];
+    }
+
     public void SetSelectedDefender(Defender defender)
     {
         this.defender = defender;
@@ -11,12 +19,35 @@ public class DefenderSpawner : MonoBehaviour
 
     private void OnMouseDown()
     {
-        Instantiate(defender, GetCellCoordinatesFromMouseClick() + defender.DefenderOffset, Quaternion.identity);
+        if (defender)
+        {
+            var cell = GetCellCoordinatesFromMouseClick();
+            if (CanPlaceDefenderInCell(cell))
+            {
+                Instantiate(defender, cell + defender.DefenderOffset, Quaternion.identity);
+                SetSellValue(cell);
+            }
+        }
     }
 
     private Vector2 GetCellCoordinatesFromMouseClick()
     {
         var position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         return new Vector2(Mathf.RoundToInt(position.x), Mathf.RoundToInt(position.y));
+    }
+
+    private void SetSellValue(Vector2 cell)
+    {
+        cells[CellCoordinatesToInt(cell)] = true;
+    }
+
+    private bool CanPlaceDefenderInCell(Vector2 cell)
+    {
+        return !cells[CellCoordinatesToInt(cell)];
+    }
+
+    private int CellCoordinatesToInt(Vector2 cell)
+    {
+        return (int)((cell.x - 1) + (cell.y - 1) * fieldSize.x);
     }
 }
