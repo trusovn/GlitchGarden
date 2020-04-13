c9b2addcef6d3f30a0b675d1fcccdaa056c1b5d3
Nikolai Trusov <trusovn@yahoo.com>
2020-04-13 12:58:33 +0300

    Fix Cactus inflicting damage animation.

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -27,7 +27,7 @@ public class Health : MonoBehaviour
         } 
         else
         {
-            visualSoundCharacterEffects.PlayDamageEffects();
+            visualSoundCharacterEffects.PlayDamageEffects(points <= minorDamageThreshold);
         }
     }
 
