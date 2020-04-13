709bc26949e6c6c2b88da77beac226a4c36b7d02
Nikolai Trusov <trusovn@yahoo.com>
2020-04-13 15:05:42 +0300

    Take DamageDealer out of Defender class

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -1,83 +1,10 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Defender : MonoBehaviour
 {
     [SerializeField] int starCost = 50;
     [SerializeField] Vector2 defenderOffset;
-    [SerializeField] GameObject attackProjectile = default;
-    [SerializeField] Vector2 projectileSpawnOffset = default;
-    [SerializeField] int attackPoints = default;
-    [SerializeField] float attackDistance = default;
-    [SerializeField] string attackAnimationParam = "Attacking";
-    [SerializeField] string attackersLayerName = "Attacker";
-
-    Animator animator;
 
     public Vector2 DefenderOffset { get => defenderOffset; set => defenderOffset = value; }
     public int StarCost { get => starCost; }
-
-    private void Start()
-    {
-        animator = GetComponent<Animator>();
-    }
-
-    public void Attack()
-    {
-        if (attackProjectile)
-        {
-            SpawnProjectile();
-        }
-        else
-        {
-            HitAttackers();
-        }
-    }
-
-    private void HitAttackers()
-    {
-        var enemies = GetEnemiesInRange<Health>();
-        foreach (var enemy in enemies)
-        {
-            enemy.GotDamage(attackPoints);
-        }
-    }
-
-    private void SpawnProjectile()
-    {
-        var projectile = Instantiate(attackProjectile, transform.position + new Vector3(projectileSpawnOffset.x, projectileSpawnOffset.y, 0), Quaternion.identity);
-        projectile.GetComponent<Projectile>().DamagePoints = attackPoints;
-    }
-
-    private void FixedUpdate()
-    {
-        if (attackDistance < 0) { return; } // Trophy, gravestone, etc.
-
-        if (CheckEnemyInAttackRange())
-        {
-            SetAttacking(true);
-        }
-        else
-        {
-            SetAttacking(false);
-        }
-    }
-
-    private bool CheckEnemyInAttackRange()
-    {
-        var hit = Physics2D.Raycast(transform.position, Vector2.right, distance: attackDistance, layerMask: LayerMask.GetMask(attackersLayerName));
-        return hit;
-    }
-
-    private void SetAttacking(bool canAttack)
-    {
-        animator.SetBool(attackAnimationParam, canAttack);
-    }
-
-    private List<T> GetEnemiesInRange<T>()
-    {
-        var enemies = Physics2D.OverlapBoxAll(new Vector2(transform.position.x, transform.position.y), new Vector2(1, 1), 0, LayerMask.GetMask(attackersLayerName));
-        return new List<T>(enemies.Select(e => e.gameObject.GetComponent<T>()));
-    }
 }
