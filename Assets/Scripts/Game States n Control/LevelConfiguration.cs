1e7a2e3d98e82535a8c628125e91ed73a8a98741
Nikolai Trusov <trusovn@yahoo.com>
2020-04-20 18:41:30 +0300

    Adding level progress slider

---
+++ Assets/Scripts/Game States n Control/LevelConfiguration.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class LevelConfiguration : ScriptableObject
+{
+    [SerializeField] float levelTime = default;
+
+    public float LevelTime { get => levelTime; }
+}
---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+    [SerializeField] LevelConfiguration levelConfiguration = default;
+
+    [Header("Handle animation settings")]
+    [SerializeField] RectTransform sliderHandle = default;
+    [SerializeField] float scaleSize = default;
+    [SerializeField] float scaleSpeed = default;
+
+    private void Start()
+    {
+        SetHandleAnimation();
+    }
+
+    private void SetHandleAnimation()
+    {
+        LeanTween.scale(sliderHandle, sliderHandle.localScale * scaleSize, 1 / scaleSpeed).setEaseInOutBounce().setLoopPingPong();
+    }
+
+    private void Update()
+    {
+        GetComponent<Slider>().value = Time.timeSinceLevelLoad / levelConfiguration.LevelTime;
+    }
+}
