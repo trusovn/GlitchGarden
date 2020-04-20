ee7a2c0b684ccf64521751f3b217231c44d02c1c
Nikolai Trusov <trusovn@yahoo.com>
2020-04-20 15:45:36 +0300

    Lose game with attacker passing the end line. Scenes transition animation.

---
+++ Assets/Scripts/LoseGame.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class LoseGame : MonoBehaviour
+{
+    [SerializeField] LevelLoader levelLoader = default;
+    [SerializeField] RectTransform sceneFadeImage = default;
+    [SerializeField] float sceneFadeTime = default;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        LaunchGameOver();
+    }
+
+    private void LaunchGameOver()
+    {
+        LeanTween.alpha(sceneFadeImage, 1, sceneFadeTime);
+        LeanTween.delayedCall(sceneFadeTime, levelLoader.LoadGameOver);
+    }
+}
---
+++ Assets/Scripts/SceneLoadEffects.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SceneLoadEffects : MonoBehaviour
+{
+    [SerializeField] RectTransform sceneFadeImage = default;
+    [SerializeField] float fadeInTime = default;
+
+    void Start()
+    {
+        StartCoroutine(FadeImageOutWithDelay());
+    }
+
+    IEnumerator FadeImageOutWithDelay()
+    {
+        yield return null;
+        LeanTween.alpha(sceneFadeImage, 0, fadeInTime);
+    }
+}
