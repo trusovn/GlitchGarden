f2c19a9d47218d8e163c7d5a113709f9ec2df698
Nikolai Trusov <trusovn@yahoo.com>
2020-04-23 11:26:22 +0300

    Adding Options screen. Completed.

---
+++ Assets/Scripts/Game States n Control/GameSettings.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class GameSettings : ScriptableObject
+{
+    [SerializeField][Range(1, 5)] int defaultDifficulty = 1;
+    [SerializeField] [Range(0, 1)] float defaultSoundVolume = .1f;
+    public float DifficultyLevel { get; set; }
+    public float SoundVolume { get; set; }
+    public int DefaultDifficulty { get => defaultDifficulty; }
+    public float DefaultSoundVolume { get => defaultSoundVolume; }
+
+    public void RestoreDefaults()
+    {
+        DifficultyLevel = defaultDifficulty;
+        SoundVolume = defaultSoundVolume;
+    }
+}
---
+++ Assets/Scripts/Game States n Control/SettingsController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsController : MonoBehaviour
+{
+    [SerializeField] GameSettings gameSettings = default;
+    [SerializeField] Slider difficulty = default;
+    [SerializeField] Slider soundVolume = default;
+
+    void Start()
+    {
+        SetSliderValues();
+    }
+
+    private void SetSliderValues()
+    {
+        difficulty.value = gameSettings.DifficultyLevel;
+        soundVolume.value = gameSettings.SoundVolume;
+    }
+
+    public void LoadDefaults()
+    {
+        gameSettings.RestoreDefaults();
+        SetSliderValues();
+    }
+}
