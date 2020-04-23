0b74e6b2bb8cfb980c499551bf32f3519ab7eb77
Nikolai Trusov <trusovn@yahoo.com>
2020-04-23 13:25:20 +0300

    Options sliders change colors based on set value. Added events.

--- Assets/Scripts/Game States n Control/GameSettings.cs
+++ Assets/Scripts/Game States n Control/GameSettings.cs
@@ -3,10 +3,30 @@
 [CreateAssetMenu]
 public class GameSettings : ScriptableObject
 {
-    [SerializeField][Range(1, 5)] int defaultDifficulty = 1;
+    [SerializeField] [Range(1, 5)] int defaultDifficulty = 1;
     [SerializeField] [Range(0, 1)] float defaultSoundVolume = .1f;
-    public float DifficultyLevel { get; set; }
-    public float SoundVolume { get; set; }
+    [SerializeField] GameEvent settingsChangedEvent = default;
+    private float soundVolume;
+    private float difficultyLevel;
+
+    public float DifficultyLevel
+    {
+        get => difficultyLevel;
+        set
+        {
+            difficultyLevel = value;
+            settingsChangedEvent.Raise();
+        }
+    }
+    public float SoundVolume
+    {
+        get => soundVolume;
+        set
+        {
+            soundVolume = value;
+            settingsChangedEvent.Raise();
+        }
+    }
     public int DefaultDifficulty { get => defaultDifficulty; }
     public float DefaultSoundVolume { get => defaultSoundVolume; }
 
--- Assets/Scripts/Game States n Control/SettingsController.cs
+++ Assets/Scripts/Game States n Control/SettingsController.cs
@@ -4,8 +4,12 @@ using UnityEngine.UI;
 public class SettingsController : MonoBehaviour
 {
     [SerializeField] GameSettings gameSettings = default;
+    [Space]
     [SerializeField] Slider difficulty = default;
     [SerializeField] Slider soundVolume = default;
+    [Space]
+    [SerializeField] Color emptyFillColor = Color.white;
+    [SerializeField] Color fullFillColor = Color.white;
 
     void Start()
     {
@@ -16,6 +20,19 @@ public class SettingsController : MonoBehaviour
     {
         difficulty.value = gameSettings.DifficultyLevel;
         soundVolume.value = gameSettings.SoundVolume;
+        SetFillColors();
+    }
+
+    public void SetFillColors()
+    {
+        SetSliderFillColor(difficulty);
+        SetSliderFillColor(soundVolume);
+    }
+
+    private void SetSliderFillColor(Slider slider)
+    {
+        var newColor = Color.Lerp(emptyFillColor, fullFillColor, slider.value / slider.maxValue);
+        slider.gameObject.transform.Find("Fill Area").Find("Fill").GetComponent<Image>().color = newColor;
     }
 
     public void LoadDefaults()
