ea3bd4c5502a625528e9c16a8bec92b541c39db0
Nikolai Trusov <trusovn@yahoo.com>
2020-04-30 22:07:43 +0300

    Continue work on LevelConfiguration inspector UI. WIP.

---
+++ Assets/Scripts/Editor/LevelDifficultyDrawer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+[CustomPropertyDrawer(typeof(LevelDifficulty))]
+public class LevelDifficultyDrawer : PropertyDrawer
+{
+    public override VisualElement CreatePropertyGUI(SerializedProperty property)
+    {
+        var container = new VisualElement();
+        container.Add(new Label("Difficulty " + property.propertyPath));
+        container.Add(new Label("Difficulty " + property.type));
+        container.Add(new Label("Difficulty " + ((LevelConfiguration)property.serializedObject.targetObject).levelDifficulties.Length));
+        //container.Add(new PropertyField())        
+        container.Add(new PropertyField(property.FindPropertyRelative("startStarsCount")));
+        var attackers = property.FindPropertyRelative("levelAttackers");
+        container.Add(new Label("Attackers size: " + attackers.arraySize.ToString()));
+        var header = new VisualElement();
+        header.style.display = DisplayStyle.Flex;
+        header.style.flexDirection = FlexDirection.Row;
+        header.style.justifyContent = Justify.SpaceAround;
+        header.Add(new Label("Attacker"));
+        header.Add(new Label("Spawn Time and Weight"));
+        container.Add(header);
+        for (int i = 0; i < attackers.arraySize; i++)
+        {
+            container.Add(new PropertyField(attackers.GetArrayElementAtIndex(i)));
+        }
+        return container;
+    }
+}
--- Assets/Scripts/Game States n Control/LevelConfiguration.cs
+++ Assets/Scripts/Game States n Control/LevelConfiguration.cs
@@ -6,7 +6,7 @@ public class LevelConfiguration : ScriptableObject
     [Rename("Level timer, seconds")]
     [SerializeField] float levelTime = default; // TODO: this is set in levelDifficulties
     [SerializeField] [Range(1, 5)] int currentDifficulty;
-    [SerializeField] LevelDifficulty[] levelDifficulties;
+    [SerializeField] public LevelDifficulty[] levelDifficulties; // TODO: remove public. Accessed in LevelDifficultyDrawer.
 
     float timeLeft;
 
